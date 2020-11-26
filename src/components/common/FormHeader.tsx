0e41b2a2c0a95007d0fa91e28cdc19a3055eacac
Selvedin <selvedinh@gmail.com>
2020-11-26 18:13:42 +0100

    adding loader on pdf file download

--- src/components/common/FormHeader.tsx
+++ src/components/common/FormHeader.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FaPlusCircle, FaList, FaFilePdf, FaEdit, FaEye } from 'react-icons/fa'
-import PdfDocument from 'components/common/PdfDocument'
-import { PDFDownloadLink } from '@react-pdf/renderer'
 import { IPriprema } from 'types/Priprema'
 import axios from 'axios'
 import FileDownload from 'js-file-download'
+import Loading from './Loading'
 
 interface FormProps {
   priprema: IPriprema,
@@ -14,13 +13,17 @@ interface FormProps {
 }
 
 const FormHeader = (props: FormProps) => {
-  const { id, priprema, isView } = props
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const { id, isView } = props
 
   const getPdf = async () => {
     try {
+      setIsLoading(true)
       const response = await axios.get(`${process.env.REACT_APP_BASE_PATH}/exports/pdf`,
         { responseType: 'blob', params: { name: 'Priprema', id: 'somestring' } })
       FileDownload(response.data, 'export.pdf')
+      setIsLoading(false)
 
     } catch (error) {
       console.log(error.response.data, 'Greska', 'error')
@@ -30,27 +33,32 @@ const FormHeader = (props: FormProps) => {
     <div className="row justify-content-md-center p-3 border-bottom">
       <div className="col">
         <h2 className='float-left'>Priprema</h2>
-        <button className='btn btn-default' onClick={getPdf}>Export</button>
-        <PDFDownloadLink className='text-danger float-right' document={<PdfDocument priprema={priprema} />} fileName={`priprema-${priprema.predmet}-${priprema.razred}-${priprema.nastavnaJedinica}`}>
-          {({ blob, url, loading, error }) => (loading ? '' : <FaFilePdf size={22} />)}
-        </PDFDownloadLink>
-        <Link className='text-primary btn-sm float-right mx-1' to='/priprema' title='Nova priprema'>
-          <FaPlusCircle size={22} />
-        </Link>
-        <Link className='text-secondary btn-sm float-right mx-1' to='/pripreme' title='Sve pripreme'>
-          <FaList size={22} />
-        </Link>
-        {
-          isView ?
-            <Link className='text-success btn-sm float-right mx-1' to={'/priprema-edit/' + id} title='Uredi pripremu'>
-              <FaEdit size={22} />
-            </Link>
-            :
-            <Link className='text-info btn-sm float-right mx-1' to={'/priprema/' + id} title='Detalji pripreme'>
-              <FaEye size={22} />
-            </Link>
+        {!isLoading ?
+          (
+            <Fragment>
+              <a href='#' className='text-danger btn-sm float-right' onClick={getPdf} title='Exportuj u PDF'><FaFilePdf size={22} /></a>
+              <Link className='text-primary btn-sm float-right mx-1' to='/priprema' title='Nova priprema'>
+                <FaPlusCircle size={22} />
+              </Link>
+              <Link className='text-secondary btn-sm float-right mx-1' to='/pripreme' title='Sve pripreme'>
+                <FaList size={22} />
+              </Link>
+              {
+                isView ?
+                  <Link className='text-success btn-sm float-right mx-1' to={'/priprema-edit/' + id} title='Uredi pripremu'>
+                    <FaEdit size={22} />
+                  </Link>
+                  :
+                  <Link className='text-info btn-sm float-right mx-1' to={'/priprema/' + id} title='Detalji pripreme'>
+                    <FaEye size={22} />
+                  </Link>
+              }
+            </Fragment>
+          )
+          : <Loading className='float-right' />
         }
         <div className='clearfix' />
+
       </div>
     </div>
   )
--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react'
 import Loader from 'react-loader-spinner'
 
-const Loading = () => {
+const Loading = (props: any) => {
   return (
     <Fragment>
       <Loader
+        {...props}
         type="ThreeDots"
         color="#00BFFF"
         height={50}
